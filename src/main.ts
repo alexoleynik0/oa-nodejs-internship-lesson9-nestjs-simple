817594e99a5c2a21454f84f0db177852323b14e4
Alex Oleynik <oleynik.webdev@gmail.com>
2022-08-25 15:18:58 +0300

    chore(app): moved app module; test: updated all; chore: api prefix added to all routes; chore: URI versioning enabled, v1 as default;

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,14 @@
+import { VersioningType } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { AppModule } from './app.module';
+import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
+  app.enableVersioning({
+    defaultVersion: '1',
+    type: VersioningType.URI,
+  });
   await app.listen(3000);
 }
 bootstrap();
