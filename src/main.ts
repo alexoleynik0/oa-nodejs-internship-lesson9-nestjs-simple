e628d9f5b11eb328bd757954e61b88d3915f1e32
Alex Oleynik <oleynik.webdev@gmail.com>
2022-08-26 03:04:48 +0300

    chore: helmet added;

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,15 @@
 import { VersioningType } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { HttpAdapterHost, NestFactory } from '@nestjs/core';
+import helmet from 'helmet';
 
 import { AllExceptionsFilter } from './filters/all-exceptions.filter';
 import { AppModule } from './modules/app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.use(helmet());
 
   app.setGlobalPrefix('api');
 
